b7d441564068571c61510384a5fa33aae827463d
Bhargavi Kurri <k.bhargavi.reddy@gmail.com>
2014-07-13 19:01:53 -0400

    Entire solution for Test Program

---
+++ TestProgram/TestProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+ class Program
+    {
+        public static void NumberOperations(int i)
+        {
+            if (i % 3 == 0 && i % 5 == 0) { Console.WriteLine("ModernMedical"); }
+            else if (i % 3 == 0) { Console.WriteLine("Modern"); }
+            else if (i % 5 == 0) { Console.WriteLine("Medical"); }
+            else Console.WriteLine(i);
+        }
+        static void Main(string[] args)
+        {
+            for(int number = 1; number <= 100; number++)
+            {
+                NumberOperations(number);
+            }
+            Console.ReadKey();
+        }
+    }
+
